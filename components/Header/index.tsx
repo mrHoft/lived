8f83bc226f77ac9e10c48588e70091ba0f080ad8
mrHoft <ru.hoft@gmail.com>
2023-09-20 08:00:12 +0300

    fix: Header styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,31 +1,32 @@
 import styles from './header.module.css'
 import { useDomainByUrl } from 'hooks/useDomain'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 const DOMAIN = process.env.DOMAIN
 
 export default function Header() {
   const router = useRouter()
   const url = DOMAIN || router.basePath
-  const subDomain = useDomainByUrl()
-  console.log(subDomain)
+  const [sub] = useState(useDomainByUrl())
+  console.log(sub, sub === 'auth-lived')
 
   return (
     <header className={styles.header}>
       <nav>
         <ul>
           <li>
-            <a href={`https://auth-lived.${url}`} className={subDomain === 'auth-lived' ? styles.current : undefined}>
+            <a href={`https://auth-lived.${url}`} className={sub === 'auth-lived' ? styles.current : undefined}>
               auth
             </a>
           </li>
           <li>
-            <a href={`https://internal-lived.${url}`} className={subDomain === 'internal-lived' ? styles.current : undefined}>
+            <a href={`https://internal-lived.${url}`} className={sub === 'internal-lived' ? styles.current : undefined}>
               internal
             </a>
           </li>
           <li>
-            <a href={`https://public-lived.${url}`} className={subDomain === 'public-lived' ? styles.current : undefined}>
+            <a href={`https://public-lived.${url}`} className={sub === 'public-lived' ? styles.current : undefined}>
               public
             </a>
           </li>
